8257276d508f5969d9691505629fdbe0b3cc5abf
nephisha <emmanuel.nephisha@gmail.com>
2017-08-09 00:45:52 +1000

    Initial Commit of my API Tests

---
+++ DataTransferObjects.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI.ClinicalTrials.APITests
+{
+    public class Dates
+    {
+        public string Acronym { get; set; }
+        public int SiteTarget { get; set; }
+        public int Participants { get; set; }
+        public object Maintainers { get; set; }
+        public string LastMaintainedBy { get; set; }
+        public string LastMaintainedDate { get; set; }
+        public object Status { get; set; }
+        public string HRECApprovalDate { get; set; }
+        public string SiteHRECApprovalDate { get; set; }
+        public bool RGONotApplicable { get; set; }
+        public string RGOSubmissionDate { get; set; }
+        public string RGOApprovalDate { get; set; }
+        public string RecruitmentOpenDate { get; set; }
+        public string FirstPatientDate { get; set; }
+        public string RecruitmentClosedDate { get; set; }
+        public string FollowUpCompleteDate { get; set; }
+        public string StudyClosureDate { get; set; }
+        public string SuspendedDate { get; set; }
+        public string AbandonedDate { get; set; }
+        public string ExtendedSubmissionDueDate { get; set; }
+        public string ReportPeriodEndDate { get; set; }
+        public bool CanArchive { get; set; }
+        public bool CanRemove { get; set; }
+        public int Percentage { get; set; }
+        public string CreatedOn { get; set; }
+        public string PerformanceFundingPaymentStartDate { get; set; }
+        public int TargetRecruitmentType { get; set; }
+        public int TargetRecruitmentTimePeriod { get; set; }
+        public object TargetRecruitmentMinRange { get; set; }
+        public object TargetRecruitmentMaxRange { get; set; }
+    }
+
+    public class CTUEmployee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Details
+    {
+        public string Investigator { get; set; }
+        public string KeyContact { get; set; }
+        public CTUEmployee CTUEmployee { get; set; }
+    }
+
+    public class SiteResponsibilityLookupItem
+    {
+        public int SiteResponsibilityLookupItemId { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+        public int CheckedValue { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+
+    public class OtherCTU
+    {
+        public string Value { get; set; }
+        public string Selected { get; set; }
+        public string Disabled { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class CurrentPeriod
+    {
+        public object ReportingPeriod { get; set; }
+        public int ReportingPeriodId { get; set; }
+        public object Prescreened { get; set; }
+        public object ScreenFailures { get; set; }
+        public object NewEnrollments { get; set; }
+        public object ActiveOnIntervention { get; set; }
+        public object CompletedIntervention { get; set; }
+        public object FollowUp { get; set; }
+        public object Discontinued { get; set; }
+        public object ReportPeriodBeginDate { get; set; }
+        public object ReportPeriodEndDate { get; set; }
+    }
+
+    public class Cumulative
+    {
+        public object ReportingPeriod { get; set; }
+        public int ReportingPeriodId { get; set; }
+        public object ReportPeriodBeginDate { get; set; }
+        public object ReportPeriodEndDate { get; set; }
+    }
+
+    public class Historical
+    {
+        public string ReportingPeriod { get; set; }
+        public int ReportingPeriodId { get; set; }
+        public int Prescreened { get; set; }
+        public int ScreenFailures { get; set; }
+        public int NewEnrollments { get; set; }
+        public int ActiveOnIntervention { get; set; }
+        public int CompletedIntervention { get; set; }
+        public int FollowUp { get; set; }
+        public int Discontinued { get; set; }
+        public string ReportPeriodBeginDate { get; set; }
+        public string ReportPeriodEndDate { get; set; }
+    }
+
+    public class PriorActivity
+    {
+        public object ReportingPeriod { get; set; }
+        public int ReportingPeriodId { get; set; }
+        public object Prescreened { get; set; }
+        public object ScreenFailures { get; set; }
+        public object NewEnrollments { get; set; }
+        public object ActiveOnIntervention { get; set; }
+        public object CompletedIntervention { get; set; }
+        public object FollowUp { get; set; }
+        public object Discontinued { get; set; }
+        public object ReportPeriodBeginDate { get; set; }
+        public object ReportPeriodEndDate { get; set; }
+    }
+
+    public class Activity
+    {
+        public int CurrentCTUId { get; set; }
+        public List<SiteResponsibilityLookupItem> SiteResponsibilityLookupItems { get; set; }
+        public int SiteResponsibilityLookupItemId { get; set; }
+        public List<OtherCTU> OtherCTUs { get; set; }
+        public List<object> SelectedOtherCTUs { get; set; }
+        public object FollowUpPatients { get; set; }
+        public CurrentPeriod CurrentPeriod { get; set; }
+        public Cumulative Cumulative { get; set; }
+        public List<Historical> Historical { get; set; }
+        public PriorActivity PriorActivity { get; set; }
+        public string BeginsOnDate { get; set; }
+        public string EndsOnDate { get; set; }
+    }
+
+    public class CTUEmployee2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SiteTargetTimePeriodLookup
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class SiteTargetTypeLookup
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class Disable
+    {
+        public bool SiteHRECApprovalDate { get; set; }
+        public bool RGOSubmissionDate { get; set; }
+        public bool RGOApprovalDate { get; set; }
+        public bool RecruitmentOpenDate { get; set; }
+        public bool FirstPatientDate { get; set; }
+        public bool SiteTarget { get; set; }
+    }
+
+    public class Tooltip
+    {
+        public string HRECApproval { get; set; }
+        public string SiteHRECApproval { get; set; }
+        public string SiteTarget { get; set; }
+        public string RGOSubmission { get; set; }
+        public string RGOAuthorisation { get; set; }
+        public string RecruitmentOpen { get; set; }
+        public string FirstPatientOnTrial { get; set; }
+        public string RecruitmentClosed { get; set; }
+        public string FollowUpComplete { get; set; }
+        public string StudyClosure { get; set; }
+        public string PreScreened { get; set; }
+        public string Participants { get; set; }
+        public string ScreenFailures { get; set; }
+        public string NewEnrolments { get; set; }
+        public string ActiveOnIntervention { get; set; }
+        public string CompletedIntervention { get; set; }
+        public string FollowUp { get; set; }
+        public string Discontinued { get; set; }
+        public string TrialSuspend { get; set; }
+        public string TrialAbandoned { get; set; }
+        public string RGONotApplicable { get; set; }
+    }
+
+    public class UI
+    {
+        public bool IsAbandoned { get; set; }
+        public bool IsSuspended { get; set; }
+        public bool CanAbandon { get; set; }
+        public bool CanSuspend { get; set; }
+        public int CTUEmployeeId { get; set; }
+        public int IsAdmin { get; set; }
+        public Disable Disable { get; set; }
+        public Tooltip Tooltip { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int Id { get; set; }
+        public bool IsEditable { get; set; }
+        public bool IsSignedOff { get; set; }
+        public bool IsArchived { get; set; }
+        public Dates Dates { get; set; }
+        public Details Details { get; set; }
+        public Activity Activity { get; set; }
+        public List<CTUEmployee2> CTUEmployees { get; set; }
+        public List<object> ValidationErrors { get; set; }
+        public List<SiteTargetTimePeriodLookup> SiteTargetTimePeriodLookups { get; set; }
+        public List<SiteTargetTypeLookup> SiteTargetTypeLookups { get; set; }
+        public bool IsIncludeCurrentPeriodData { get; set; }
+        public UI UI { get; set; }
+    }
+}
---
+++ TrialDatesValidationTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestSharp;
+
+namespace CI.ClinicalTrials.APITests
+{
+    class TrialDatesValidationTest
+    {
+        [Test]
+        public void verify_FirstPatient_Trialdate_must_be_later_than_startdate_of_firstEnrollment()
+        {
+            var dateValidationRequestPayload = new List<RootObject>
+            {
+                new RootObject
+                {
+                    Activity = new Activity
+                    {
+                        BeginsOnDate = "26/03/2017",
+                        CurrentCTUId = 53,
+                        EndsOnDate = "31/10/2017",
+                        SiteResponsibilityLookupItemId = 1
+                    },
+                    Dates = new Dates
+                    {
+                        AbandonedDate = "",
+                        Acronym = "Rapid Pharmacovigilance",
+                        CanArchive = false,
+                        CanRemove = false,
+                        CreatedOn = "11/07/2016",
+                        ExtendedSubmissionDueDate = "",
+                        FirstPatientDate = "02/09/2011",
+                        FollowUpCompleteDate = "",
+                        HRECApprovalDate = "",
+                        LastMaintainedBy = "CTU User",
+                        LastMaintainedDate = "17/01/2017",
+                        Maintainers = null,
+                        Participants = 10,
+                        Percentage = 100,
+                        PerformanceFundingPaymentStartDate = "01/01/2016",
+                        RGOApprovalDate = "13/04/2011",
+                        RGONotApplicable = false,
+                        RGOSubmissionDate = "30/03/2011",
+                        RecruitmentClosedDate = "",
+                        RecruitmentOpenDate = "01/09/2011",
+                        ReportPeriodEndDate = "31/10/2017",
+                        SiteHRECApprovalDate = "20/03/2011",
+                        SiteTarget = 10,
+                        Status = null,
+                        StudyClosureDate = "",
+                        SuspendedDate = "",
+                        TargetRecruitmentMaxRange = null,
+                        TargetRecruitmentMinRange = null,
+                        TargetRecruitmentTimePeriod = 1,
+                        TargetRecruitmentType = 1
+                    }
+                    ,
+                    CTUEmployees = new List<CTUEmployee2>
+                    {
+                        new CTUEmployee2
+                        {
+                            Id = 12737,
+                            Name = "Test User"
+                        }
+                    },
+                    Details = new Details
+                    {
+                        CTUEmployee = new CTUEmployee
+                        {
+                            Id = 949,
+                            Name = "Mary Perry"
+                        },
+                        Investigator = "Caitlin Sheehan",
+                        KeyContact = "Mary Perry"
+                    },
+                    Id = 4625,
+                    IsArchived = false,
+                    IsEditable = true,
+                    IsIncludeCurrentPeriodData = false,
+                    IsSignedOff = false,
+                    SiteTargetTimePeriodLookups = new List<SiteTargetTimePeriodLookup>
+                    {
+
+                    },
+                    SiteTargetTypeLookups = new List<SiteTargetTypeLookup>
+                    {
+
+                    },
+                    UI = new UI
+                    {
+
+                    }
+                }
+            };
+            var json = JsonConvert.SerializeObject(dateValidationRequestPayload);
+            Console.WriteLine(json);
+
+            var restClient = new RestClient("http://uat-clinicaltrials.cancerinstitute.org.au/");
+            var restRequest = new RestRequest(Method.POST) { Resource = "MySite/EditSiteTrial/4625" };
+            restRequest.AddHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+            restRequest.AddHeader("Content-Type", "application/json");
+            restRequest.AddHeader("Cookie",
+                "CurrentUserCTUId-F3E448AC-E587-4C89-86C3-9DAACC697E28=; CurrentUserCTUName-F3E448AC-E587-4C89-86C3-9DAACC697E28=; user.id=10621; user.lastlogindate=07/08/2017; user.displayname=Emmanuel Russel; .ASPXAUTH=D2214097514FEF69AF1A576B513E006300A342FCBD1F1E4A84DF02DB32104D78CBE8DEC4B3E64E53436268E166E9BB25057D2EEA4D7F8326B71185801A1A81F1B62A1AA0450E18A010F231A84AC46AF08C7B41825C7833D129ED161D5827FB931C408D7A; ASP.NET_SessionId=qjworo00n4ewu0zmnu2amgae; user.ctucount=61; user.ctuids=,2,60,64,3,42,53,51,10,49,12,13,4,67,48,38,23,22,6,21,20,61,45,52,57,63,71,7,5,58,65,66,8,28,9,54,29,72,56,14,15,16,17,36,18,24,19,27,43,47,50,26,37,30,31,44,55,62,32,33,34,35,; X-Qlik-Session-ctp-uat=qjworo00n4ewu0zmnu2amgae; __RequestVerificationToken=ErBetRPlQEJF9x3udm-ZMF2buQlLZ_EXdwrteJBuGka9_YdfexBtUC0quS2-oCGXoQYP9tLnzxue0ftcJSjNHy_Ts1M1; user.currentctuid=53; _ga=GA1.3.928026109.1502104832; _gid=GA1.3.667905507.1502104832");
+            restRequest.AddParameter("application/json",json,ParameterType.RequestBody);
+            var response = restClient.Execute(restRequest);
+            response.StatusCode.Equals(HttpStatusCode.OK);
+        }
+    }
+}
